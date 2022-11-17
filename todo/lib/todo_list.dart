8ec2e3b404099ab7cb32a7d35dcbb98fbd4704a7
Abdulsalam <mr.smady@gmail.com>
2022-11-17 10:55:01 +0200

    https://jsonplaceholder.typicode.com/guide/

--- todo/lib/todo_list.dart
+++ todo/lib/todo_list.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:todo/main.dart';
+import 'package:http/http.dart' as http;
 
 import 'models/todo.dart';
 
@@ -47,14 +50,17 @@ class _TodoListState extends State<TodoList> {
           },
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            insert(
-              Todo(
-                note: 'Test 1',
-                creationDate: DateTime.now(),
-              ),
-            );
-            updateTodoList();
+          onPressed: () async {
+            var url = Uri.https('jsonplaceholder.typicode.com', 'posts');
+            var response = await http.post(url,
+                body: jsonEncode({
+                  "userId": 1,
+                  "title":
+                      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                  "body":
+                      "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                }));
+            print(response.statusCode);
           },
           child: const Icon(
             Icons.add,
